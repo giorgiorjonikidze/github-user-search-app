57e0fcd846a3a2291891dcae02680ea629df6745
WINDOWS-9CI3A45\User <gioorjo123@gmail.com>
2022-10-20 10:05:33 +0400

    created pfoject setup

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    screens: {
+      md: "768px",
+      xl: "1440px",
+    },
+    colors: {
+      "blue": "#0079FF",
+      'light-gray': "#697C9A",
+      "light-dark-blue": "#4B6A9B",
+      "light-very-dark-blue": "#2B3442",
+      "light-bg-white": "#F6F8FF",
+      "dark-black-black": "#141D2F",
+      "dark-black-blue": "#1E2A47",
+      "light-font-color": "#4B6A9B",
+      "dark-font-color": "#2B3442",
+      "allert-color": "#F74646",
+
+    },
+    fontFamily: {
+      spacemono: ['Space Mono']
+    },
+    borderColor: {
+      "sm": "10px",
+      "md": "15px"
+    }
+    
+  },
+  plugins: [],
+}
